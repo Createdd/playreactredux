bb07ff38b5bb9ae944704840b525c71102cc4534
Daniel Deutsch <deudan1010@gmail.com>
2017-02-07 17:38:16 +0100

    initial

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const Root = () => {
+  return (
+    <div>hello</div>
+  )
+};
+
+render(<Root/>, document.querySelector('#main'));
