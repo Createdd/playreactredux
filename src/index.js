1cc47b9dfdf6f2d68d71a70fb30946e024ac04f9
Daniel Deutsch <deudan1010@gmail.com>
2017-02-08 11:24:53 +0100

    inital with react-scripts

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 
-const Root = () => {
-  return (
-    <div>hello</div>
-  )
-};
-
-render(<Root/>, document.querySelector('#main'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
